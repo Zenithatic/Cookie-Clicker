bb178e220fad49ed31e1a70cc5b3591d4ab84e77
Zenithatic <patmc55555@gmail.com>
2022-06-09 19:03:09 -0400

    Added buttons to LoadPanel
    Load buttons and Reset buttons are now on the LoadPanel with no functionality yet. Two new mouselistener classes have been created for them as well.

---
+++ src/listeners/LoadListener.java
@@ -0,0 +1,49 @@
+package listeners;
+import java.awt.Image;
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.*;
+
+/**
+ * 
+ * @author Patrick Wang, Andrew Yoo
+ * @apiNote The custom MouseListener for the load buttons
+ * 
+ */
+public class LoadListener implements MouseListener{
+	// implement these methods to fulfill abstraction
+    public void mouseClicked(MouseEvent e){}
+    public void mouseReleased(MouseEvent e){}
+    
+    public void mousePressed(MouseEvent e){
+
+    }
+    
+    public void mouseEntered(MouseEvent e){
+        // replace Image Icon of load button with selected form
+        JButton loadButton = (JButton) e.getSource();
+        try {
+            // load and apply image
+            Image loadImage = ImageIO.read(new File("assets\\loadSelected.png")).getScaledInstance(300, 150, java.awt.Image.SCALE_SMOOTH);
+            loadButton.setIcon(new ImageIcon(loadImage));
+        } catch (IOException exception) {
+            // output traced error
+            exception.printStackTrace();
+        }
+    }
+
+    public void mouseExited(MouseEvent e){
+    	// replace Image Icon of load button with non-selected form
+        JButton loadButton = (JButton) e.getSource();
+        try {
+            // load and apply image
+            Image loadImage = ImageIO.read(new File("assets\\load.png")).getScaledInstance(300, 150, java.awt.Image.SCALE_SMOOTH);
+            loadButton.setIcon(new ImageIcon(loadImage));
+        } catch (IOException exception) {
+            // output traced error
+            exception.printStackTrace();
+        }
+    }
+}
